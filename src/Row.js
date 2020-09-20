8dc9034d8b1e631742dacfab7b1e9eb0254a29c8
Loucif RAKEM <loucif.rakem@edu.univ-fcomte.fr>
2020-09-20 20:41:12 +0200

    added main banner

--- src/Row.js
+++ src/Row.js
@@ -3,6 +3,7 @@ import axios from "./axios";
 import "./Row.css";
 
 const baseUrl = "https://image.tmdb.org/t/p/original/";
+
 function Row({ title, fetchUrl, isLarge }) {
   const [movies, setmovies] = useState([]);
 
@@ -15,22 +16,24 @@ function Row({ title, fetchUrl, isLarge }) {
     fetchData();
   }, [fetchUrl]);
 
-  //   console.log(movies);
-
   return (
     <div className="row">
       <h2>{title}</h2>
       <div className="row_posters">
-        {movies.map((movie) => (
-          <img
-            key={movie.id}
-            className={`row_poster ${isLarge && "row_posterLarger"}`}
-            src={`${baseUrl}${
-              isLarge ? movie.poster_path : movie.backdrop_path
-            }`}
-            alt={movie.name}
-          />
-        ))}
+        {movies.map((movie) => {
+          if (movie.poster_path !== null && movie.backdrop_path !== null)
+            return (
+              <img
+                key={movie.id}
+                className={`row_poster ${isLarge && "row_posterLarger"}`}
+                src={`${baseUrl}${
+                  isLarge ? movie.poster_path : movie.backdrop_path
+                }`}
+                alt={movie.name}
+              />
+            );
+          else return null;
+        })}
       </div>
     </div>
   );
