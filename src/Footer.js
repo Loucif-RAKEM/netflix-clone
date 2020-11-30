ab02a7abf78034dcde2c50d925c222a099d98d20
Loucif RAKEM <loucif.rakem@edu.univ-fcomte.fr>
2020-11-30 22:09:57 +0100

    updated footer style

--- src/Footer.js
+++ src/Footer.js
@@ -2,6 +2,7 @@ import React from "react";
 import MailIcon from "@material-ui/icons/Mail";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import GitHubIcon from "@material-ui/icons/GitHub";
+import FavoriteIcon from "@material-ui/icons/Favorite";
 
 import "./Footer.css";
 
@@ -9,7 +10,9 @@ function Footer() {
   return (
     <footer className="footer">
       <span>
-        Developped by <span className="name">Loucif RAKEM</span> in 2020
+        2020, Developed with{" "}
+        <FavoriteIcon fontSize="small" style={{ color: "red" }} /> by{" "}
+        <span className="name">Loucif RAKEM</span>
       </span>
       <a href="https://www.themoviedb.org">
         <img
@@ -21,13 +24,13 @@ function Footer() {
 
       <div className="buttons">
         <a href="https://www.linkedin.com/in/lrakem/">
-          <LinkedInIcon className="footer_icon" fontSize="large" />
+          <LinkedInIcon className="footer_icon" />
         </a>
         <a href="https://github.com/Loucif-RAKEM/netflix-clone">
-          <GitHubIcon className="footer_icon" fontSize="large" />
+          <GitHubIcon className="footer_icon" />
         </a>
         <a href="mailto:rakem.loucif@gmail.com">
-          <MailIcon className="footer_icon" fontSize="large" />
+          <MailIcon className="footer_icon" />
         </a>
       </div>
     </footer>
